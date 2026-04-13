69cac8da77120904609224680ce1a029e1b1c899
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-04-14 00:04:21 +0500

    dd

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -141,6 +141,7 @@ class BookingController extends Controller
     // Handle Point to Point form submission
     public function handlePointToPoint(Request $request)
     {
+        dd($request->all());
         session([
             'booking_completed' => false
         ]);
